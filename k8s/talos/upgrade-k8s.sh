596eed43d3a730e1cc32b9cc11587a4543ff629e
Emerson Connelly <emerconn7@gmail.com>
2026-04-26 06:52:00 -0500

    Update upgrade-k8s.sh

--- k8s/talos/upgrade-k8s.sh
+++ k8s/talos/upgrade-k8s.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/bash
 
-K8S_VERSION="1.35.1"
+VERSION="1.35.1"
 
-talosctl upgrade-k8s -n cp-01 --to "$K8S_VERSION" || {
-  echo "Upgrade Kubernetes to $K8S_VERSION failed" >&2
+talosctl upgrade-k8s -n cp-01 --to "$VERSION" || {
+  echo "K8s $VERSION upgrade failed" >&2
 }
