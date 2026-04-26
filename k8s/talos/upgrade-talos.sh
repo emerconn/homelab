0108161ba9bc5bfd9dbdea18077d51d15a65121b
Emerson Connelly <emerconn7@gmail.com>
2026-04-26 06:54:48 -0500

    Update upgrade-talos.sh

--- k8s/talos/upgrade-talos.sh
+++ k8s/talos/upgrade-talos.sh
@@ -1,14 +1,14 @@
 #! /usr/bin/bash
 
-# update this version
-TALOS_VERSION="1.12.4"
-
-IMAGE="factory.talos.dev/metal-installer/0b4f48281e59712995bea152e8e62f3082be4ab66d2bdd0ca83cb3ce8c4509a9:v$TALOS_VERSION"
+VERSION="1.12.4"
 NODES=(cp-01 cp-02 cp-03 w-01)
 
-for node in "${NODES[@]}"; do
-  echo "Upgrading $node..."
-  talosctl upgrade -i "$IMAGE" -n "$node" || {
-    echo "Upgrade Talos to $TALOS_VERSION failed for $node" >&2
+# 4 extensions
+IMAGE="factory.talos.dev/metal-installer/0b4f48281e59712995bea152e8e62f3082be4ab66d2bdd0ca83cb3ce8c4509a9:v$VERSION"
+
+for n in "${NODES[@]}"; do
+  echo "Upgrading $n to $VERSION..."
+  talosctl upgrade -i "$IMAGE" -n "$n" || {
+    echo "Upgrade failed for $n" >&2
   }
 done
